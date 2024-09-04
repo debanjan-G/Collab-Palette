23d292b0eed7a8dd22c668bba88ca1ab1b05e275
Debanjan <debanjanghosal10@gmail.com>
2024-09-04 21:31:32 +0530

    Created Frontend Project Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px] ">
+      <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
+
+      <div className="h-screen flex justify-center items-center">
+        <h1 className="text-5xl font-extrabold ">Real-Time Collaborative Whiteboard</h1>
+      </div>
+    </div >
+  );
+}
+
+export default App;
