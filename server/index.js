d81d2851a8220df68d672a0d8457b2814e158c70
Debanjan <debanjanghosal10@gmail.com>
2024-09-09 11:18:02 +0530

    Added and Updated whiteboardData and userJoined Event handler

--- server/index.js
+++ server/index.js
@@ -16,12 +16,46 @@ app.get("/", (req, res) => {
   );
 });
 
+let imageURL, userRoom;
 io.on("connection", (socket) => {
+  // When a new user joins the room
   socket.on("userJoined", (data) => {
-    console.log("DATA = ", data);
+    console.log("userJoined Server Event Handler Fired!!!");
+
     const { roomID, userID, name, host, presenter } = data;
+    console.log("ROOM ID = ", roomID);
+
+    // Store the roomID in the socket object
+    userRoom = roomID;
+    console.log("userRoom = ", userRoom);
+
+    // Socket joins the specified room
     socket.join(roomID);
+
+    // Notify the user that they have successfully joined
     socket.emit("userIsJoined", { success: true });
+
+    // Send the latest whiteboard image to the newly joined user
+    socket.emit("whiteboardDataResponse", {
+      updatedImage: imageURL,
+    });
+  });
+
+  // When whiteboard data is updated
+  socket.on("whiteboardData", (stageImage) => {
+    console.log("whiteboardData Server Handler fired!");
+
+    // Store the latest whiteboard image globally
+    imageURL = stageImage;
+
+    console.log("ROOM ID", userRoom);
+
+    if (userRoom) {
+      socket.broadcast.to(userRoom).emit("whiteboardDataResponse", {
+        updatedImage: stageImage,
+      });
+      console.log("RESPONSE SENT successfully!");
+    }
   });
 });
 
